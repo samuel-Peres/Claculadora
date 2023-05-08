073edaf64dfa25a9277c0c3e18b8b624fdd9f851
Samuel Martisn Peres <samuelmartinsperes2001@gamil.com>
2023-05-08 11:11:05 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+
+let button_input = document.querySelectorAll(".input-button");
+
+let input = document.getElementById("input");
+let ingual = document.getElementById("ingual");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("apagar");
+
+window.onload = () => {
+  input.value = "0"
+
+};
+
+button_input.forEach((button_class) => {
+  button_class.addEventListener("click", () => {
+    input.value += button_class.value;
+  });
+});
+
+ingual.addEventListener("click", () => {
+  ingual_pressed = 1;
+  let inp_val = input.value;
+  try {
+    let solucao = eval(inp_val);
+    if (Number.isInteger(solucao)) {
+      input.value = solucao;
+    } else {
+      input.value = solucao.toFixed(2);
+    } 
+  } catch (err) {
+    alert("invalido")
+  }
+});
+
+clear.addEventListener("click", () => {
+  input.value = "0";
+});
+
+erase.addEventListener("click", () => {
+  input.value = input.value.substr(0, input.value.length - 1);
+  if(input.value.substr(0, input.value.length - 1) < 1) {
+    input.value = "0"
+  }
+});
+    
